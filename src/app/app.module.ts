6459aeee7e5a36f966f3bec9b77453f6774ef453
afolabiopakunle <opakunleafolabi@gmail.com>
2021-07-20 08:36:11 +0100

    Handmade component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 ;
-import { ServersComponent } from './servers/servers.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ServerComponent, ServersComponent ],
+  declarations: [ AppComponent, WarningAlertComponent, SuccessAlertComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
